65ea17008069c9f969766796cbb6463efc0d6cc1
Mark Henrich <mark.henrich@icloud.com>
2023-11-11 17:43:52 -0700

    Add @CrossOrigin for local api calls to work

--- src/main/java/com/github/markahenrich/nasaopenapi/controller/ApodController.java
+++ src/main/java/com/github/markahenrich/nasaopenapi/controller/ApodController.java
@@ -2,12 +2,14 @@ package com.github.markahenrich.nasaopenapi.controller;
 
 import com.github.markahenrich.nasaopenapi.model.Apod;
 import com.github.markahenrich.nasaopenapi.service.ApodService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/apod")
+@CrossOrigin
 public class ApodController {
     private final ApodService apodService;
 
