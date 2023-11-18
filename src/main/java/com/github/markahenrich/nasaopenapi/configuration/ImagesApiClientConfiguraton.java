b7a5608d947537aa03c17edc48789d76f73edbea
Mark Henrich <mark.henrich@icloud.com>
2023-11-18 01:40:09 -0700

    Fix exceeded limit error, increase max in memory size

--- src/main/java/com/github/markahenrich/nasaopenapi/configuration/ImagesApiClientConfiguraton.java
+++ src/main/java/com/github/markahenrich/nasaopenapi/configuration/ImagesApiClientConfiguraton.java
@@ -4,19 +4,24 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ImagesApiClientConfiguraton {
     @Value("${nasa.images.url}")
     private String baseUrl;
+    final int size = 16 * 1024 * 1024;
 
     @Bean
     @Qualifier("images")
     public WebClient getImagesWebclient() {
-        return WebClient
-                .builder()
+        return WebClient.builder()
                 .baseUrl(baseUrl)
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                        .build())
                 .build();
     }
 }
+
